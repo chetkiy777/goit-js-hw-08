820d6aaf0420fc299e858135a8828fc854d757b9
Igor Gladkiy <egunka1988@gmail.com>
2021-11-29 13:38:25 +0200

    done part2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery')
+
+
+    const addGallery = (imgArray) => {
+    return imgArray.map(({ preview, description, original }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href=${original}>
+                <img src=${preview} title=${description}  alt=${description}  data-source=${original} class="gallery__image">
+            </a>
+        </div>`;
+    }).join('')
+    }
+
+    let cardSet = addGallery(galleryItems)
+    galleryList.insertAdjacentHTML('afterbegin',cardSet )
+    
+var lightbox = new SimpleLightbox('.gallery a', {
+    
+    overlay: true,
+    captionDelay: '250ms'
+});
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const player = new Player('vimeo-player', {
+    width: 640
+})
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.on('timeupdate', throttle(() => {
+    player.getCurrentTime().then(res => localStorage.setItem("videoplayer-current-time", res))
+}, 1000))
+
+function setPrevTime() {
+    let savedTime = localStorage.getItem("videoplayer-current-time")
+    player.setCurrentTime(savedTime)
+}
+
+document.addEventListener('DOMContentLoaded',  setPrevTime())
